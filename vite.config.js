17d94adcfeb773d290e46bda5a7041bdb253b489
chunglijung <andy28572463@gmail.com>
2023-08-26 21:25:36 +0800

    init

---
+++ vite.config.js
@@ -0,0 +1,62 @@
+import { defineConfig, loadEnv } from "vite";
+import { ViteEjsPlugin } from "vite-plugin-ejs";
+import { fileURLToPath } from "node:url";
+import path from "node:path";
+import { glob } from "glob";
+
+import liveReload from "vite-plugin-live-reload";
+
+function moveOutputPlugin() {
+  return {
+    name: "move-output",
+    enforce: "post",
+    apply: "build",
+    async generateBundle(options, bundle) {
+      for (const fileName in bundle) {
+        if (fileName.startsWith("pages/")) {
+          const newFileName = fileName.slice("pages/".length);
+          bundle[fileName].fileName = newFileName;
+        }
+      }
+    },
+  };
+}
+
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    // base 的寫法：
+    // base: '/Repository 的名稱/'
+    base: env.VITE_BASE_URL,
+    plugins: [
+      liveReload([
+        "./layout/**/*.ejs",
+        "./components/**/*.ejs",
+        "./pages/**/*.ejs",
+        "./pages/**/*.html",
+      ]),
+      ViteEjsPlugin(),
+      moveOutputPlugin(),
+    ],
+    server: {
+      // 啟動 server 時預設開啟的頁面
+      open: env.VITE_SERVER_URL + "index.html",
+    },
+    build: {
+      rollupOptions: {
+        input: Object.fromEntries(
+          glob
+            .sync("pages/**/*.html")
+            .map((file) => [
+              path.relative(
+                "pages",
+                file.slice(0, file.length - path.extname(file).length)
+              ),
+              fileURLToPath(new URL(file, import.meta.url)),
+            ])
+        ),
+      },
+      outDir: "dist",
+    },
+  };
+});
